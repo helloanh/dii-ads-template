70308ed33d4666e350395d0fce8f20df497f5734
helloanh <ahoang10@jhu.edu>
2019-07-17 14:43:00 -0400

    update to have fixed height cards

---
+++ html-template/vue-app.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+  	selected: '',
+    portfolios: [
+      {id: 'CAI', name: 'CAI'},
+      {id: 'CMS', name: 'CMS'},
+      {id: 'HRIS', name: 'HRIS'},
+      {id: 'VADDI', name: 'VADDI'},
+    ]
+  }
+})
+
+
