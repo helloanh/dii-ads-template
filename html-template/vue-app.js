c82435e132c0327cec0daae0eb6b6b0a9adf23b7
helloanh <ahoang10@jhu.edu>
2019-07-18 09:48:04 -0400

    update

--- html-template/vue-app.js
+++ html-template/vue-app.js
@@ -3,10 +3,11 @@ var app = new Vue({
   data: {
   	selected: '',
     portfolios: [
-      {id: 'CAI', name: 'CAI'},
-      {id: 'CMS', name: 'CMS'},
-      {id: 'HRIS', name: 'HRIS'},
-      {id: 'VADDI', name: 'VADDI'},
+      {id: 'Corporate Services (116)', name: 'Corporate Services (116)', info: 'More info 1'},
+      {id: 'Benefits and Memorial Services (75)', name: 'Benefits and Memorial Services (75)', info: 'More info 2' },
+      {id: 'Enterprise Business Services (75)', name: 'Enterprise Business Services (75)', info: 'More info 3' },
+      {id: 'Health Services (296)', name: 'Health Services (296)', info: 'More info 4' },
+      {id: 'Technology Services (157)', name: 'Technology Services (157)', info: 'More info 5' }
     ]
   }
 })
